719e72e8b5aeb985b2062712c57e64bb90ef2643
Reinaldo Zezela <rzezela@gmail.com>
2020-04-17 03:40:42 +0200

    insert comparison tab

---
+++ server/03_serverComparisonUI.R
@@ -0,0 +1,16 @@
+##############################################################
+## 03_serverComparisonUI.R
+#############################################################
+
+
+output$comparisonUI <- renderUI({
+    
+    tagList(
+        tags$h1("Under Construction ...")
+        
+        # argonR::argonRow(
+        #     
+        #     argonH1("Under Construction ...", display = 1) %>% argonTextColor(color = 'white')
+        # )
+    )
+})
