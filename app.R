8d2bf9c3086b4cb0136809a0c93370d88cd33ffa
Reinaldo Zezela <rzezela@gmail.com>
2020-07-26 15:56:43 +0200

    app in R Shiny

--- app.R
+++ app.R
@@ -1,20 +1,22 @@
-if(interactive()){
-    library(shiny)
-    library(argonR)
-    library(argonDash)
+
+    # library(shiny)
+    # library(argonR)
+    # library(argonDash)
+    # 
+    # library(tidyverse)
+    # library(shinycssloaders) 
+    # library(shinyWidgets)
     
-    library(tidyverse)
-    library(shinycssloaders)
-    library(shinyWidgets)
+    
+
+    source(file = 'getFunctions.R', local = TRUE)
+
+    source(file = 'global.R', local = TRUE)
     
     
+    source(file = 'ui/header.R', local = TRUE)
+    source(file = 'ui/body.R', local = TRUE)
     
-    source(file = 'global.R')
-    source(file = 'getFunctions.R')
-    
-    source(file = 'ui/header.R')
-    source(file = 'ui/body.R')
-    source(file = 'ui/dashboardUI.R')
     # source(file = 'ui/sidebar.R')
     
     # source(file = 'CSS.R')
@@ -42,7 +44,8 @@ if(interactive()){
                 HTML(
                     paste(
                         "2020,", 
-                        a(href = "https://reinaldozezela.netlify.com/", "Tchingue powered", target = "_blank")
+                        a(href = "https://reinaldozezela.netlify.com/", "powered by Tchingue", target = "_blank"),
+                        img(src = "image/logoTchingue_v4.PNG", height = "40")
                     )
                 )
             )
@@ -55,10 +58,10 @@ if(interactive()){
             source(file = "server/01_srv_dashboard_COVID19.R", local = TRUE)
             
             source(file = "server/02_serverWorldMapUI.R", local = TRUE)
-            
+
             source(file = "server/03_serverComparisonUI.R", local = TRUE)
-            
+
             source(file = "server/04_serverForecastUI.R", local = TRUE)
         }
     )
-}
+
